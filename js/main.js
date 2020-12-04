23ac63a79de6539036575a15ebed109e33b8977f
phuockg <quangphuoc@jv-it.com.vn>
2020-12-04 16:55:19 +0700

    create layout landing page

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+$( ".section-3__listQA-title__wrap" ).each(function( index ) {
+    let tag = this;
+    $(this).find('.section-3__listQA-title').on("click",function(){
+        $(tag).find('.section-3__listQA').slideToggle()
+    })
+  });
