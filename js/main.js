4099dbbd3dbd64693e9ee42c0d15a0d3fd18a824
nguyen phuoc <quangphuoc@jv-it.com.vn>
2020-12-08 22:49:38 +0700

    create

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-
-$( ".section-3__listQA-title__wrap" ).each(function( index ) {
-    let tag = this;
-    $(this).find('.section-3__listQA-title').on("click",function(){
-        $(tag).find('.section-3__listQA').slideToggle()
-    })
-  });
+$(".section-3__listQA-title__wrap").each(function (index) {
+  let tag = this;
+  $(this)
+    .find(".section-3__listQA-title")
+    .on("click", function () {
+      $(tag).find(".section-3__listQA").slideToggle();
+    });
+    
+});
